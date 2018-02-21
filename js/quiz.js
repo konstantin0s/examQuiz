c16c53ed0a39717bbf53247743c3bd302e0ce8a1
konstantin0s <constantintofan85@gmail>
2018-02-21 21:16:07 +0100

    Add questions to javascript file

---
+++ js/quiz.js
@@ -0,0 +1,8 @@
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+var questions = [
+     ["What is 10 + 4?", "12", "14", "16", "B"],
+     ["What is 20 - 9?", "7", "13", "11", "C"],
+     ["What is 7 * 3?", "21", "24", "25", "A"],
+     ["What is 8 / 2?", "10", "2", "14", "C"]
+
+];
